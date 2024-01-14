9af74477deba58b1d48e9ebc6678faa4eeb71123
anirudh-r5 <anirudh.r515@gmail.com>
2024-01-14 14:13:36 -0800

    fixed DNS error
    Signed-off-by: anirudh-r5 <anirudh.r515@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -198,15 +198,15 @@ resource "aws_cloudfront_distribution" "dist" {
   depends_on = [aws_acm_certificate_validation.cert]
 }
 
-# resource "cloudflare_record" "site_cname" {
-#   zone_id = data.cloudflare_zones.domain.zones[0].id
-#   name    = var.site_domain
-#   value   = aws_cloudfront_distribution.dist.domain_name
-#   type    = "CNAME"
+resource "cloudflare_record" "site_cname" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  name    = var.site_domain
+  value   = aws_cloudfront_distribution.dist.domain_name
+  type    = "CNAME"
 
-#   ttl     = 1
-#   proxied = true
-# }
+  ttl     = 1
+  proxied = true
+}
 
 resource "cloudflare_record" "www" {
   zone_id = data.cloudflare_zones.domain.zones[0].id
@@ -216,4 +216,12 @@ resource "cloudflare_record" "www" {
 
   ttl     = 1
   proxied = true
+}
+
+resource "cloudflare_page_rule" "https" {
+  zone_id = data.cloudflare_zones.domain.zones[0].id
+  target  = "http://*.${var.site_domain}/*"
+  actions {
+    always_use_https = true
+  }
 }
